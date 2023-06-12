d8963b26493708a4ada185f4a97e754d423b5f05
guneyunay <mesafe18@hotmail.com>
2023-06-12 19:46:15 +0300

    form control with js

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Sign Up Form
+
+This a challenge for build one page web which user should be able to:
+
++ View the optimal layout for the component depending on their device's screen size
+
++ It checks the entered mail and if it is wrong, it gives a notification to the user.
+
++ -Semantic HTML5 markup
+
++ CSS custom properties
+
++ Flexbox
+
+## Whad did i learn? 
+
+I first learned how to create an HTML structure and within that structure, I learned to take input from a form element and use JavaScript to validate that input and provide feedback to the user. I saw where I had gaps in my knowledge and enjoyed the process of doing it.
+
+![Project-İmage](./screenshots/Ekran%20g%C3%B6r%C3%BCnt%C3%BCs%C3%BC%202023-06-12%20193506.png)
+
---
+++ script.js
@@ -0,0 +1,36 @@
+const input = document.getElementById('email')
+const error = document.getElementById('error')
+const inputArea = document.getElementById('email')
+const paragraph = document.getElementById('success-text')
+
+
+
+function getValidate(){
+    let email = input.value
+    let emailRegex =  /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let isValidEmail = emailRegex.test(email);
+    localStorage.setItem('email' , email)
+
+    if(isValidEmail){
+       window.location.href = 'success.html'
+ 
+ 
+
+    } else{
+        error.classList.add('active')
+        inputArea.style.border = '1px solid red'
+        inputArea.style.background = '#F9716C'
+
+    }
+
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let email = localStorage.getItem('email');
+    if (email) {
+        let successText = document.getElementById('success-text');
+        successText.innerHTML = `A confirmation email has been sent to <b>${email}</b>. Please open it and click the button inside to confirm your subscription.`;
+    } else{
+        successText.innerHTML = `Beklenmedik bir hata oluştu.`;
+    }
+});
